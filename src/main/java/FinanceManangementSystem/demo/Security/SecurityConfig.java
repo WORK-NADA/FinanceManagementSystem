bafe4ce1aacb656a3795019c31b6c624d505ec76
HarshGit11 <harshnada11@gmail.com>
2026-09-07 23:43:44 +0530

    Latest Backend 07-09-2026/22:43

--- src/main/java/FinanceManangementSystem/demo/Security/SecurityConfig.java
+++ src/main/java/FinanceManangementSystem/demo/Security/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -49,6 +50,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/", "/health", "/api/health").permitAll()
                         .requestMatchers("/user/login").permitAll()
                         .requestMatchers("/user/me").hasAnyAuthority("ADMIN","CLIENT")
